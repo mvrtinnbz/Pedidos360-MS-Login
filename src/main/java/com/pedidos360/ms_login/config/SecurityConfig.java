9552e26e1d79c22aaca35b15090e48c19b7f1479
mvrtinnbz <martinbazas2004@gmail.com>
2026-09-09 02:40:26 -0300

    Version 1.3.0: Agrega Dockerfile para despliegue en EC2

--- src/main/java/com/pedidos360/ms_login/config/SecurityConfig.java
+++ src/main/java/com/pedidos360/ms_login/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+            .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
 
             .sessionManagement(session ->
